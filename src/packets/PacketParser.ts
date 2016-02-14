b34c3994872cbaee51a261e8d1dd7d65a502053b
asarium <asarium@gmail.com>
2016-02-14 13:40:33 +0100

    Add parser for server start packets

--- src/packets/PacketParser.ts
+++ src/packets/PacketParser.ts
@@ -31,6 +31,11 @@ export function PacketParser() {
                 case Identifiers.PCKT_SLIST_REQUEST_FILTER:
                     this.int32le("type").int32le("status").string("filter");
                     break;
+                case Identifiers.PCKT_SERVER_START:
+                    this.string("name").string("mission_name").string("title").string("campaign_name").uin8le("campaign_mode")
+                        .int32le("flags").int32le("type_flags").int16le("num_players").int16le("max_players").uint8le("mode")
+                        .uint8le("rank_base").uint8le("game_state").uint8le("connection_speed").string("tracker_channel");
+                    break;
                 default:
                     // Consume all data, maybe someone else can do something with it...
                     this.buffer("data", this.vars.length - 5); // subtract 5 to account for id and length
