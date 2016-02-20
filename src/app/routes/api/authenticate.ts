4a0eb7c461331560c8643c9213b57574d276c2a6
asarium <asarium@gmail.com>
2016-02-20 16:12:11 +0100

    Fix authentication and add utility function

---
+++ src/app/routes/api/authenticate.ts
@@ -0,0 +1,90 @@
+import * as express from "express";
+import {RouterContext} from "../../WebInterface";
+import {Router} from "express";
+import * as passport from "passport";
+import * as config from "config";
+import {Authentication} from "../../../util/Authentication";
+import * as jwt from "jwt-simple";
+import * as winston from "winston";
+
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+const JWT_SECRET = config.get<string>("web.jwt.secret");
+
+export = function (context: RouterContext): Router {
+    let router = express.Router();
+
+    router.post("/authenticate", (req, res) => {
+        let username = req.body.user;
+        let password = req.body.password;
+
+        if (typeof username !== "string" || typeof password !== "string") {
+            res.status(400).json({
+                                     err: "Invalid parameters"
+                                 });
+            return null;
+        }
+
+        context.Database.Models.User.find({
+                                              where: {
+                                                  Username: username
+                                              }
+                                          }).then(user => {
+            if (user == null) {
+                res.status(401).json({
+                                         err: "Invalid authentication"
+                                     });
+                return;
+            }
+
+            return Authentication.verifyPassword(user, password).then(valid => {
+                if (!valid) {
+                    res.status(401).json({
+                                             err: "Invalid authentication"
+                                         });
+                    return;
+                }
+
+                let user_data = {
+                    id: user.get("id")
+                };
+                let token = jwt.encode(user_data, JWT_SECRET);
+
+                res.status(200).json({
+                                         token: "JWT " + token
+                                     });
+            });
+        }).catch(err => {
+            winston.error("Error while authenticating client.", err);
+            res.status(500).json({
+                                     err: "Internal server error"
+                                 });
+        });
+    });
+
+    let jwtOptions = {
+        secretOrKey: config.get<string>("web.jwt.secret"),
+        jwtFromRequest: ExtractJwt.fromAuthHeader(),
+    };
+
+    passport.use(new JwtStrategy(jwtOptions, (payload, done) => {
+        if (typeof payload.id !== "number") {
+            return done(null, false);
+        }
+
+        context.Database.Models.User.findById(payload.id).then(user => {
+            if (user == null) {
+                done(null, false);
+                return;
+            }
+            done(null, user);
+        }).catch(err => {
+            done(err, false);
+        });
+    }));
+
+    router.use(passport.initialize());
+
+    return router;
+}
--- src/app/routes/api/index.ts
+++ src/app/routes/api/index.ts
@@ -1,16 +1,15 @@
-
 import * as express from "express";
 import * as bodyParser from "body-parser";
 import {RouterContext} from "../../WebInterface";
 import {Router} from "express";
 
-export = function(context: RouterContext): Router {
+export = function (context: RouterContext): Router {
     let router = express.Router();
 
-    router.use(bodyParser.urlencoded({ extended: true }));
+    router.use(bodyParser.urlencoded({extended: true}));
     router.use(bodyParser.json());
 
-    router.use("/", require("./authentication")(context));
+    router.use("/", require("./authenticate")(context));
     router.use("/servers", require("./servers")(context));
     router.use("/users", require("./users")(context));
     router.use("/onlineusers", require("./onlineusers")(context));
